1bb731b9166ca6832b83e78c888c7532e1c272bc
Lev Lybin <lev.lybin@gmail.com>
2019-05-30 02:20:09 +0700

    account blocks mined

--- scan/views.py
+++ scan/views.py
@@ -30,6 +30,14 @@ class BlockListView(ListView):
     paginate_by = 25
     ordering = '-height'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if self.request.GET.get('m'):
+            qs = qs.filter(generator_id=self.request.GET.get('m'))
+
+        return qs
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
@@ -165,6 +173,8 @@ class AddressDetailView(DetailView):
 
         obj = context[self.context_object_name]
 
+        #
+
         context['txs'] = Transaction.objects.using('java-wallet').filter(
             Q(sender_id=obj.id) | Q(recipient_id=obj.id)
         ).order_by('-height')[:15]
@@ -178,10 +188,22 @@ class AddressDetailView(DetailView):
             Q(sender_id=obj.id) | Q(recipient_id=obj.id)
         ).count()
 
+        #
+
         pool_id = get_pool_id_for_account(obj.id)
         if pool_id:
             context['pool_id'] = pool_id
             context['pool_name'] = get_account_name(pool_id)
+
+        #
+
+        context['mined_blocks'] = Block.objects.using('java-wallet').filter(
+            generator_id=obj.id
+        ).order_by('-height')[:15]
+
+        context['mined_blocks_cnt'] = Block.objects.using('java-wallet').filter(
+            generator_id=obj.id
+        ).count()
 
         return context
 
