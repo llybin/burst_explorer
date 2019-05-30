f7816e5c69dd497e9afe4f5554e9e0ea60e91d36
Lev Lybin <lev.lybin@gmail.com>
2019-05-31 01:32:34 +0700

    add multiout unpack

---
+++ burst/multiout.py
@@ -0,0 +1,38 @@
+import sys
+import logging
+import struct
+
+
+class UnpackError(Exception):
+    pass
+
+
+class MultiOut(object):
+    @staticmethod
+    def _unpack_header(data):
+        headers = struct.unpack("2c", data)
+
+        version = int.from_bytes(headers[0], byteorder=sys.byteorder)
+        if version != 1:
+            logging.warning("Unknown multiout version: %d, data: %r", version, data)
+            raise UnpackError
+
+        nums = int.from_bytes(headers[1], byteorder=sys.byteorder)
+
+        return version, nums
+
+    def unpack_multi_out(self, data):
+        try:
+            version, nums = self._unpack_header(data[:2])
+            return struct.unpack("{}P".format(2 * nums), data[2:])
+        except struct.error:
+            logging.error("Unpack error: %r", data)
+            raise UnpackError
+
+    def unpack_multi_out_same(self, data):
+        try:
+            version, nums = self._unpack_header(data[:2])
+            return struct.unpack("{}P".format(nums), data[2:])
+        except struct.error:
+            logging.error("Unpack error: %r", data)
+            raise UnpackError
