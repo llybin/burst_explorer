471f9ad5071c1d64cd5aec1dc47024697adb6c6b
Lev Lybin <lev.lybin@gmail.com>
2019-05-31 07:03:29 +0700

    issue #8 multiout in account

--- scan/views.py
+++ scan/views.py
@@ -94,7 +94,7 @@ class MultiOutListView(ListView):
     model = MultiOut
     queryset = MultiOut.objects.all()
     template_name = 'mos/list.html'
-    context_object_name = 'txs'
+    context_object_name = 'mos'
     paginator_class = CachingPaginator
     paginate_by = 25
     ordering = '-height'
@@ -125,7 +125,7 @@ class MultiOutListView(ListView):
             if t.recipient_id:
                 t.recipient_name = get_account_name(t.recipient_id)
 
-        context['txs_cnt'] = get_multiouts_count()
+        context['mos_cnt'] = get_multiouts_count()
 
         return context
 
@@ -229,7 +229,7 @@ class AddressDetailView(DetailView):
 
         obj = context[self.context_object_name]
 
-        #
+        # transactions
 
         context['txs'] = Transaction.objects.using('java-wallet').filter(
             Q(sender_id=obj.id) | Q(recipient_id=obj.id)
@@ -240,18 +240,36 @@ class AddressDetailView(DetailView):
             if t.recipient_id:
                 t.recipient_name = get_account_name(t.recipient_id)
 
+            if t.type == 0 and t.subtype in {1, 2}:
+                v, t.multiout = MultiOutPack().unpack_header(t.attachment_bytes)
+
         context['txs_cnt'] = Transaction.objects.using('java-wallet').filter(
             Q(sender_id=obj.id) | Q(recipient_id=obj.id)
         ).count()
 
-        #
+        # multiouts
+
+        context['mos'] = MultiOut.objects.filter(
+            Q(sender_id=obj.id) | Q(recipient_id=obj.id)
+        ).order_by('-height')[:15]
+
+        for t in context['mos']:
+            t.sender_name = get_account_name(t.sender_id)
+            if t.recipient_id:
+                t.recipient_name = get_account_name(t.recipient_id)
+
+        context['mos_cnt'] = MultiOut.objects.filter(
+            Q(sender_id=obj.id) | Q(recipient_id=obj.id)
+        ).count()
+
+        # pool info
 
         pool_id = get_pool_id_for_account(obj.id)
         if pool_id:
             context['pool_id'] = pool_id
             context['pool_name'] = get_account_name(pool_id)
 
-        #
+        # blocks
 
         context['mined_blocks'] = Block.objects.using('java-wallet').filter(
             generator_id=obj.id
