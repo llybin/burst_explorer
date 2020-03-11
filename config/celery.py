4d4cff7241d85842886ef2115830dd9f2dd44abc
Lev Lybin <lev.lybin@gmail.com>
2020-03-11 23:39:25 +0700

    get exchange data only for mainnet

--- config/celery.py
+++ config/celery.py
@@ -17,10 +17,6 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
-app.conf.beat_schedule = {
-    "get_exchange_info": {"task": "scan.tasks.get_exchange_info", "schedule": 60}
-}
-
 # https://docs.celeryproject.org/en/latest/userguide/routing.html
 app.conf.task_default_queue = "celery"
 app.conf.task_queues = (Queue("celery"),)
--- scan/tasks.py
+++ scan/tasks.py
@@ -1,8 +1,16 @@
+from django.conf import settings
+
 from config import celery_app
 from scan.helpers.exchange import cache_exchange_data, get_exchange_data
 
 
-@celery_app.task(time_limit=5)
+@celery_app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    if not settings.TEST_NET:
+        sender.add_periodic_task(60, get_exchange_info)
+
+
+@celery_app.task(time_limit=10)
 def get_exchange_info():
     data = get_exchange_data()
     cache_exchange_data(data)
